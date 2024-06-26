c95ea00a073e22ba77b5e829e66031739944307d
Anas Laghrissi <108026572+anaslaghrissi@users.noreply.github.com>
2024-06-26 16:49:12 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".button");
+button.addEventListener("click", (e) => {
+  e.preventDefault;
+  button.classList.add("animate");
+  setTimeout(() => {
+    button.classList.remove("animate");
+  }, 600);
+});
